b4ad230f73613ac78b38d1099fced5ac3e2050e4
ssllaavv <ssllaavv@gmail.com>
2024-04-10 09:13:20 +0300

    change the URL in UI tests to produxtion URL

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8080/Add-Product');
+    await page.goto('https://ssllaavv-exam-shopping-list.onrender.com/Add-Product');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
